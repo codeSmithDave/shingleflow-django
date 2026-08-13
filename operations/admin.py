323e90c8e5d6b1b6e9a5869b8e6c7b474a8cea19
Dave <davidpuinean@gmail.com>
2026-08-13 14:50:56 -0600

    Add Client, Project, WorkDay, Inquiry models with computed status/overdue logic

--- operations/admin.py
+++ operations/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Client, Project, WorkDay
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Project)
+admin.site.register(WorkDay)
