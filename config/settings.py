a7cfe3c89fe048664e9c7cb8fd0f25c68ddcccb0
Dave <29952471+codeSmithDave@users.noreply.github.com>
2026-08-21 12:53:34 -0600

    generate new Django secret key in .env

--- config/settings.py
+++ config/settings.py
@@ -11,17 +11,19 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z3#(0aos^!(kw@dh$a#uce(v5x3_0_gb^i(mc(7i+vuci16hd6'
-
+SECRET_KEY = env("SECRET_DJANGO_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
